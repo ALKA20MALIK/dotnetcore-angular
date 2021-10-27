b6f7addc81a82d96b7ddf1caa2352de8850eca2e
bhokumar <bhoopendra.akgec@gmail.com>
2021-10-27 10:59:09 +0530

    End of Section 5

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,13 +1,10 @@
 
-using System.Text;
 using API.Extentions;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace API
